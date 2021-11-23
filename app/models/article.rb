ac6272c80bee056c89c3e0664ab1a0915ecd2b74
dbpatnodee <dbpatnode@hotmail.com>
2021-11-23 10:45:00 -0800

    added validations to article model

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ApplicationRecord
+    # checks if title is there and that the length isn't to long/short
+    validates :title, presence: true, length: {minimum: 6, maximimum: 100}
+    validates :description, presence: true, length: {minimum: 10, maximimum: 5000}
+
+end
