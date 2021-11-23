8fd96858d21f1c22d4d91e7db48988c40d90732b
dbpatnodee <dbpatnode@hotmail.com>
2021-11-23 12:46:29 -0800

    fixed routes to show all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,15 @@
 class ArticlesController < ApplicationController
-    
+
 
     def show
         # having an instance variable makes it available in in the view
         # params is how you grab ids and such
         @article = Article.find(params[:id])
+    end
+
+    def index 
+        # grabs all of the articles
+        @articles = Article.all
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  get 'articles', to: 'articles#show'
-
-  resources :articles, only: [:show]
+  # :index - shows all articles
+  # :show - shows individual articles
+  resources :articles, only: [:show, :index]
 end
