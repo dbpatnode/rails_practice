008b4bf800cce0a1a3986f830950aead99786517
dbpatnodee <dbpatnode@hotmail.com>
2021-11-28 15:31:28 -0800

    added validation for email to make sure its lower cased before being added to the data base

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    # makes all emails lowercased before being added to the database.
+    before_save { self.email = email.downcase}
     has_many :articles
     # uniquness: {case_sesnitive:false} ensures even if someone enters the same email with capitols that an error message will be thrown
     validates :username, presence: true, 
